32fe92ebfaa2fcfd1dedf191d9ee486452941715
Docforce <justin@docforce.io>
2021-07-19 13:48:40 -0500

    Test 404 page errors

--- pages/404.js
+++ pages/404.js
@@ -11,16 +11,20 @@ const postType = 'page'
  * Render the Custom404 component.
  *
  * @author WebDevStudios
- * @param  {object}  props      The component attributes as props.
- * @param  {object}  props.post Post data from WordPress.
- * @return {Element}            The Custom404 component.
+ * @param  {object}  props           The component attributes as props.
+ * @param  {object}  props.post      Post data from WordPress.
+ * @param  {object}  props.restProps Other data passed in to 404.
+ * @return {Element}                 The Custom404 component.
  */
-export default function Custom404({post}) {
+export default function Custom404({post, ...restProps}) {
   const {seo, ...postData} = post
 
   // Update robots SEO meta.
   seo.metaRobotsNofollow = 'noindex'
   seo.metaRobotsNoindex = 'nofollow'
+
+  console.error('Custom404: post: ', post)
+  console.error('Custom404: restProps: ', restProps)
 
   // Display dynamic page data if 404 page retrieved from WP.
   if (postData && Object.keys(postData).length > 0) {
